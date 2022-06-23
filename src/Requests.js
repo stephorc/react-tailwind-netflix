fc66634f60a4c1b7d25d9803045c1234e957aac1
Steph <91650907+stephorc@users.noreply.github.com>
2022-06-23 09:33:38 -0400

    Fixed mixed content issue.

--- src/Requests.js
+++ src/Requests.js
@@ -6,13 +6,13 @@ const requests = {
   // requestTrending: `https://api.themoviedb.org/3/trending/all/day?api_key=${key}`,
   requestPopular: `https://api.themoviedb.org/3/movie/popular?api_key=${key}&language=en-US&page=1`,
   requestUpcoming: `https://api.themoviedb.org/3/movie/upcoming?api_key=${key}&language=en-US&page=1`,
-  requestHorror: `http://api.themoviedb.org/3/discover/movie?api_key=${key}&with_genres=27`,
-  requestThriller: `http://api.themoviedb.org/3/discover/movie?api_key=${key}&with_genres=53`,
-  requestMystery: `http://api.themoviedb.org/3/discover/movie?api_key=${key}&with_genres=9648`,
-  requestWestern: `http://api.themoviedb.org/3/discover/movie?api_key=${key}&with_genres=37`,
-  requestRomance: `http://api.themoviedb.org/3/discover/movie?api_key=${key}&with_genres=10749`,
-  requestFantasy: `http://api.themoviedb.org/3/discover/movie?api_key=${key}&with_genres=14`,
-  requestScienceFiction: `http://api.themoviedb.org/3/discover/movie?api_key=${key}&with_genres=878`,
+  requestHorror: `https://api.themoviedb.org/3/discover/movie?api_key=${key}&with_genres=27`,
+  requestThriller: `https://api.themoviedb.org/3/discover/movie?api_key=${key}&with_genres=53`,
+  requestMystery: `https://api.themoviedb.org/3/discover/movie?api_key=${key}&with_genres=9648`,
+  requestWestern: `https://api.themoviedb.org/3/discover/movie?api_key=${key}&with_genres=37`,
+  requestRomance: `https://api.themoviedb.org/3/discover/movie?api_key=${key}&with_genres=10749`,
+  requestFantasy: `https://api.themoviedb.org/3/discover/movie?api_key=${key}&with_genres=14`,
+  requestScienceFiction: `https://api.themoviedb.org/3/discover/movie?api_key=${key}&with_genres=878`,
 };
 
 export default requests;
